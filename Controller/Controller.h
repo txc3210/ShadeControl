508ca7f2b2a34612cec99ca5c0e4d2c4340a044e
Administrator <Administrator@CHAO-PC>
2018-11-17 23:17:27 +0800

    Add a module include a class named controller

---
+++ Controller/Controller.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+#include "..\ShadeControl\Serial.h"
+
+#define RS485_WAIT_TIME		200 //RS485通讯等待时间，单位 ms
+
+#define MAX_ANGLE_TIME		680.0f //百叶90度时对应的电机时间
+#define MIN_ANGLE_TIME		680.0F //百叶90度时对应的电机时间
+
+#define MAX_ANGLE	90.0f //百叶最大角度
+#define MIN_ANGLE	0.0f //百叶最小角度
+
+//控制模式
+#define MODE_AUTO		0x00 //自动控制模式
+#define MODE_MANUAL		0xFF //手动控制模式
+
+//通讯状态
+#define RS485_OK		0xF0 //通读正常
+#define RS485_OFFLINE	0xF1 //设备没有响应
+
+class controller
+{
+public:	
+	controller(CSerial *com_port, unsigned char dev_addr)
+	{
+		state = 0;//默认通讯正常
+		addr = dev_addr;
+		mode = MODE_AUTO;
+		angle = 0;
+		port = com_port;
+	}
+	~controller() {};
+	int set_angle(unsigned char angle);
+	int get_angle();
+	int set_mode(unsigned char mode);
+	int get_mode();	
+	int synchroize();
+
+	int flush(unsigned char angle, unsigned char mode);
+
+private:
+	unsigned short angle_to_time(unsigned char angle);
+	unsigned char time_to_angle(unsigned short time);
+
+public:
+	unsigned char state; //通讯状态
+	unsigned char addr; //RS485地址
+	unsigned char mode; //运行模式
+	unsigned char angle; //角度，取值0-90
+	
+private:
+	CSerial * port; //通讯串口
+	unsigned short max_angle_time; //从0到90度时电机动作时间
+	unsigned short min_angle_time; //从90到0度时电机动作时间
+};
---
+++ ShadeControl/Serial.h
@@ -0,0 +1,55 @@
+#ifndef _DLL_SAMPLE_H
+#define _DLL_SAMPLE_H
+
+// 通过宏来控制是导入还是导出
+#ifdef SERIAL_EXPORTS
+#define DLL_Serial_API __declspec(dllexport)
+#else
+#define DLL_Serial_API __declspec(dllimport)
+#endif
+
+// 导出/导入变量声明
+
+
+#define FC_DTRDSR       0x01
+#define FC_RTSCTS       0x02
+#define FC_XONXOFF      0x04
+#define ASCII_BEL       0x07
+#define ASCII_BS        0x08
+#define ASCII_LF        0x0A
+#define ASCII_CR        0x0D
+#define ASCII_XON       0x11
+#define ASCII_XOFF      0x13
+
+
+class DLL_Serial_API CSerial
+{
+	
+public:
+	int SendDelayData( /*const*/unsigned char *buffer, int size );
+	BOOL Clear();
+	CSerial();
+	~CSerial();
+	
+	BOOL Open( int nPort = 2, int nBaud = 9600 );
+	BOOL Close( void );
+	
+	int ReadData( void *, int );
+	int SendData( unsigned char *, int );
+	
+	int ReadDataWaiting( void );
+	
+	//BOOL IsOpened( void ){ return( m_bOpened ); }
+	BOOL IsOpened( void );
+	
+	BOOL WriteCommByte(unsigned char *lpCommand, DWORD dwBytesToWrite);
+	
+protected:
+	BOOL WriteCommByte( unsigned char );
+	HANDLE m_hIDComDev;
+	OVERLAPPED m_OverlappedRead, m_OverlappedWrite;
+	BOOL m_bOpened;
+	
+};
+
+#endif
